9a14ac6f549d891421b2f163bb96d1d504c272a7
ad dab <adamtareq138@example.com>
2023-09-02 16:13:01 +0300

    adding update, quote and get_account_metas including initial tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@ use anchor_lang::prelude::*;
 use anchor_spl::token::Token;
 use jupiter_amm_interface::{AccountMap, Amm, KeyedAccount, Quote, QuoteParams, Side, Swap, SwapAndAccountMetas, SwapParams, try_get_account_data};
 use solana_sdk::pubkey::Pubkey;
-use solana_sdk::reward_type::RewardType::Fee;
 
 use crate::state::{AdditionalPdaAccount, FeeMod, Market, OrderTree};
 use crate::utils::{get_additional_pda, get_fee_mod, get_market_auth_pda};
@@ -13,6 +12,8 @@ mod state;
 mod utils;
 mod constants;
 
+
+#[derive(Clone)]
 pub struct GigadexOBSwap {
     key: Pubkey,
     market: Market,
@@ -24,8 +25,9 @@ pub struct GigadexOBSwap {
 
 
 impl Amm for GigadexOBSwap {
-    fn from_keyed_account(keyed_account: &KeyedAccount) -> Result<Self> {
-        let market_state: Market = Market::from(&keyed_account.account.data[0..]);
+    fn from_keyed_account(keyed_account: &KeyedAccount) -> std::result::Result<GigadexOBSwap, anyhow::Error> {
+        let market_state: Market = Market::deserialize(&mut &keyed_account.account.data[8..])?;
+
 
         let state = GigadexOBSwap {
             key: keyed_account.key,
@@ -57,28 +59,35 @@ impl Amm for GigadexOBSwap {
     }
 
     fn get_accounts_to_update(&self) -> Vec<Pubkey> {
+        println!("asks {}", self.market.asks);
+        println!("bids {}", self.market.bids);
+        println!("fee_mod {}", get_fee_mod(self.key.clone()));
+        println!("additional_pda {}", get_additional_pda(self.key.clone()));
         vec![self.market.asks,
-             self.market.asks,
+             self.market.bids,
              get_fee_mod(self.key.clone()),
              get_additional_pda(self.key.clone())]
     }
 
-    fn update(&mut self, account_map: &AccountMap) -> Result<()> {
-        let bids_data = account_map.get(&self.market.bids).unwrap();
-        let asks_data = account_map.get(&self.market.asks).unwrap();
-        let fee_mod_data = account_map.get(&get_fee_mod(self.key.clone())).unwrap();
-        let additional_pda_data = account_map.get(&get_additional_pda(self.key.clone())).unwrap();
-        self.asks = OrderTree::from(asks_data.data.as_slice());
-        self.bids = OrderTree::from(bids_data.data.as_slice());
-        self.fee_mod = FeeMod::from(fee_mod_data.data.as_slice());
-        self.additional_pda = AdditionalPdaAccount::from(additional_pda_data.data.as_slice());
+    fn update(&mut self, account_map: &AccountMap) -> std::result::Result<(), anyhow::Error> {
+        let bids_data = try_get_account_data(account_map, &self.market.bids).unwrap();
+        let asks_data = try_get_account_data(account_map, &self.market.asks).unwrap();
+        let fee_mod_data = try_get_account_data(account_map, &get_fee_mod(self.key.clone())).unwrap();
+        let additional_pda_data = try_get_account_data(account_map, &get_additional_pda(self.key.clone())).unwrap();
+        self.asks = OrderTree::deserialize(&mut &asks_data[8..]).unwrap();
+        self.bids = OrderTree::deserialize(&mut &bids_data[8..]).unwrap();
+
+        println!("{}",self.asks.root_idx);
+        println!("{}",self.bids.root_idx);
+
+
+        self.fee_mod = FeeMod::deserialize(&mut &fee_mod_data[8..])?;
+        self.additional_pda = AdditionalPdaAccount::deserialize(&mut &additional_pda_data[8..])?;
         Ok(())
     }
 
-    fn quote(&self, quote_params: &QuoteParams) -> Result<Quote> {}
-
-    fn get_swap_and_account_metas(&self, swap_params: &SwapParams) -> Result<SwapAndAccountMetas> {
-        let buy = if swap_params.source_mint == self.market.quote_mint {
+    fn quote(&self, quote_params: &QuoteParams) -> std::result::Result<Quote, anyhow::Error> {
+        let buy = if quote_params.input_mint == self.market.quote_mint {
             true
         } else {
             false
@@ -90,11 +99,35 @@ impl Amm for GigadexOBSwap {
             &self.bids
         };
 
+        let mut amount_out = order_tree.calculate_quote(quote_params.in_amount, buy);
+        let fees = (amount_out as f64 * (self.fee_mod.base_fee_bp as f64 / 100_f64)) as u64;
+        amount_out = amount_out - fees;
+        let enough_liquidity = amount_out > 0;
+        let quote = Quote {
+            not_enough_liquidity: enough_liquidity,
+            min_in_amount: Some(quote_params.in_amount),
+            min_out_amount: Some(amount_out),
+            in_amount: quote_params.in_amount,
+            out_amount: amount_out,
+            fee_amount: fees,
+            fee_mint: self.market.quote_mint,
+            fee_pct: Default::default(),
+        };
+        Ok(quote)
+    }
+
+    fn get_swap_and_account_metas(&self, swap_params: &SwapParams) -> std::result::Result<SwapAndAccountMetas, anyhow::Error> {
+        let order_tree = if swap_params.source_mint == self.market.quote_mint {
+            &self.market.asks
+        } else {
+            &self.market.bids
+        };
+
         let accounts_metas = [
             AccountMeta::new(swap_params.user_transfer_authority, true),
             AccountMeta::new(self.key, false),
             AccountMeta::new(self.market.balances, false),
-            AccountMeta::new(self.market.asks, false),
+            AccountMeta::new(order_tree.to_owned(), false),
             AccountMeta::new(self.market.bids, false),
             AccountMeta::new(self.market.wsol_vault, false),
             AccountMeta::new(self.market.lot_vault, false),
@@ -121,5 +154,60 @@ impl Amm for GigadexOBSwap {
 
     fn get_accounts_len(&self) -> usize {
         32
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use solana_client::rpc_client::RpcClient;
+    use solana_sdk::pubkey;
+
+    use super::*;
+
+    #[test]
+    // TODO replace std::env by mainnet market after audit deploy
+    fn test_jupiter_local() -> Result<()> {
+        let market = pubkey!("b457V3EKEy6EB714Y3X4m3xHkDzbTC14iVhNfQZR6rt");
+
+        let rpc = RpcClient::new("https://rpc.hellomoon.io/28632bc6-af26-49e1-93ea-e83334aa3cb0");
+        let account = rpc.get_account(&market).unwrap();
+
+        let market_account = KeyedAccount {
+            key: market,
+            account,
+            params: None,
+        };
+
+        let mut openbook = GigadexOBSwap::from_keyed_account(&market_account).unwrap();
+
+        let pubkeys = openbook.get_accounts_to_update();
+        println!("pubkeys: {:?}", pubkeys);
+        let accounts: AccountMap = pubkeys
+            .iter()
+            .zip(rpc.get_multiple_accounts(&pubkeys).unwrap())
+            .map(|(key, acc)| (
+                *key, acc.unwrap()))
+            .collect();
+
+        openbook.update(&accounts).unwrap();
+
+        let (base_mint, quote_mint) = {
+            let reserves = openbook.get_reserve_mints();
+            (reserves[0], reserves[1])
+        };
+
+        let quote_params = QuoteParams {
+            in_amount: 80,
+            input_mint: base_mint,
+            output_mint: quote_mint,
+        };
+
+        let quote = openbook.quote(&quote_params).unwrap();
+
+        println!("{:#?}", quote_params);
+        println!("{:#?}", quote);
+
+        Ok(())
     }
 }
--- src/state.rs
+++ src/state.rs
@@ -1,9 +1,8 @@
-use solana_sdk::pubkey::Pubkey;
-use anchor_lang::prelude::*;
-use borsh::BorshSerialize;
+use anchor_lang::prelude::Pubkey;
+use borsh::{BorshDeserialize, BorshSerialize};
 
 #[repr(C)]
-#[derive(Debug, Default, PartialEq)]
+#[derive(Debug, Default, PartialEq, Clone, BorshSerialize, BorshDeserialize)]
 pub struct Market {
     pub mint: Pubkey,
     pub balances: Pubkey,
@@ -14,29 +13,7 @@ pub struct Market {
     pub quote_mint: Pubkey,
 }
 
-impl From<&[u8]> for Market {
-    fn from(data: &[u8]) -> Self {
-        let mint = Pubkey::new(&data[0..32]);
-        let balances = Pubkey::new(&data[32..64]);
-        let wsol_vault = Pubkey::new(&data[64..96]);
-        let lot_vault = Pubkey::new(&data[96..128]);
-        let asks = Pubkey::new(&data[128..160]);
-        let bids = Pubkey::new(&data[160..192]);
-        let quote_mint = Pubkey::new(&data[192..224]);
-
-        Self {
-            mint,
-            balances,
-            wsol_vault,
-            lot_vault,
-            asks,
-            bids,
-            quote_mint,
-        }
-    }
-}
-
-#[derive(Default)]
+#[derive(Clone, BorshSerialize, BorshDeserialize)]
 pub struct OrderTree {
     pub root_idx: u64,
     pub market_buy: u64,
@@ -56,6 +33,94 @@ pub struct OrderTree {
     pub amount_cancelled: u64,
 }
 
+impl Default for OrderTree {
+    fn default() -> Self {
+        Self {
+            root_idx: 0,
+            market_buy: 0,
+            nodes: [Node::default(); 1_000],
+            num_orders: 0,
+            current_signer: Default::default(),
+            remaining_amount: 0,
+            num_fills: 0,
+            fills: [FilledOrder::default(); 64],
+            num_deltas: 0,
+            node_delta: [NodeDeltaLog::default(); 64],
+            amount_cancelled: 0,
+        }
+    }
+}
+
+
+impl OrderTree {
+    pub fn calculate_quote(&self, amount_in: u64, buy: bool) -> u64 {
+        let mut remaining_amount = amount_in;
+        let mut total_received = 0;
+
+        let mut root_idx = self.root_idx as usize;
+
+        while remaining_amount > 0 {
+            let best_price = self.get_best(root_idx);
+            if best_price == 0 {
+                return 0;
+            }
+
+            let order_node = self.get(root_idx);
+            let order_amount = order_node.amount;
+            if buy {
+                if order_amount * best_price > remaining_amount {
+                    total_received += remaining_amount / best_price;
+                    break;
+                } else {
+                    total_received += order_amount;
+                    remaining_amount -= order_amount * best_price;
+                    root_idx =
+                        order_node.left as usize
+                }
+            } else {
+                if order_amount > remaining_amount {
+                    total_received += remaining_amount * best_price;
+                    break;
+                } else {
+                    total_received += order_amount * best_price;
+                    remaining_amount -= order_amount;
+                    root_idx =
+                        order_node.right as usize
+                }
+            }
+        }
+
+        // 1_000_000 is the precision of the market, constant across all markets
+        total_received / 1_000_000
+    }
+
+    fn get(&self, idx: usize) -> &Node {
+        &self.nodes[idx]
+    }
+
+    pub fn get_best(&self, root_idx: usize) -> u64 {
+        if root_idx == 0 {
+            0
+        } else if self.market_buy > 0 {
+            // get lowest
+            if self.nodes[root_idx].left > 0 {
+                self.get_best(self.nodes[root_idx].left as usize)
+            } else {
+                self.nodes[root_idx].price
+            }
+        } else {
+            // get highest
+            if self.nodes[root_idx].right > 0 {
+                self.get_best(self.nodes[root_idx].right as usize)
+            } else {
+                self.nodes[root_idx].price
+            }
+        }
+    }
+}
+
+
+#[derive(Default, Clone, Copy, PartialEq, BorshSerialize, BorshDeserialize)]
 pub struct Node {
     // key
     pub price: u64,
@@ -73,12 +138,14 @@ pub struct Node {
     pub height: u64,
 }
 
+#[derive(Default, Clone, Copy, PartialEq, BorshSerialize, BorshDeserialize)]
 pub struct FilledOrder {
     pub price: u64,
     pub amount: u64,
     pub uid: u64,
 }
 
+#[derive(Default, Clone, Copy, PartialEq, BorshSerialize, BorshDeserialize)]
 pub struct NodeDeltaLog {
     pub key: u64,
     pub is_delete: u64,
@@ -89,115 +156,7 @@ pub struct NodeDeltaLog {
     pub price: u64,
 }
 
-
-impl From<&[u8]> for Node {
-    fn from(data: &[u8]) -> Self {
-        let price = u64::from_le_bytes(data[0..8].try_into().unwrap());
-        let amount = u64::from_le_bytes(data[8..16].try_into().unwrap());
-        let uid = u64::from_le_bytes(data[16..24].try_into().unwrap());
-        let left = u64::from_le_bytes(data[24..32].try_into().unwrap());
-        let right = u64::from_le_bytes(data[32..40].try_into().unwrap());
-        let next = u64::from_le_bytes(data[40..48].try_into().unwrap());
-        let height = u64::from_le_bytes(data[48..56].try_into().unwrap());
-
-        Node {
-            price,
-            amount,
-            uid,
-            left,
-            right,
-            next,
-            height,
-        }
-    }
-}
-
-impl From<&[u8]> for FilledOrder {
-    fn from(data: &[u8]) -> Self {
-        let price = u64::from_le_bytes(data[0..8].try_into().unwrap());
-        let amount = u64::from_le_bytes(data[8..16].try_into().unwrap());
-        let uid = u64::from_le_bytes(data[16..24].try_into().unwrap());
-
-        FilledOrder { price, amount, uid }
-    }
-}
-
-impl From<&[u8]> for NodeDeltaLog {
-    fn from(data: &[u8]) -> Self {
-        let key = u64::from_le_bytes(data[0..8].try_into().unwrap());
-        let is_delete = u64::from_le_bytes(data[8..16].try_into().unwrap());
-        let is_insert = u64::from_le_bytes(data[16..24].try_into().unwrap());
-        let is_delta = u64::from_le_bytes(data[24..32].try_into().unwrap());
-        let amount = u64::from_le_bytes(data[32..40].try_into().unwrap());
-        let uid = u64::from_le_bytes(data[40..48].try_into().unwrap());
-        let price = u64::from_le_bytes(data[48..56].try_into().unwrap());
-
-        NodeDeltaLog {
-            key,
-            is_delete,
-            is_insert,
-            is_delta,
-            amount,
-            uid,
-            price,
-        }
-    }
-}
-
-impl From<&[u8]> for OrderTree {
-    fn from(data: &[u8]) -> Self {
-        let root_idx = u64::from_le_bytes(data[0..8].try_into().unwrap());
-        let market_buy = u64::from_le_bytes(data[8..16].try_into().unwrap());
-
-        let mut nodes = [Node::default(); 1_000];
-        for i in 0..1_000 {
-            let start = 16 + i * 56;
-            let end = start + 56;
-            nodes[i] = Node::from(&data[start..end]);
-        }
-
-        let num_orders = u64::from_le_bytes(data[56016..56024].try_into().unwrap());
-        let current_signer = Pubkey::new(&data[56024..56056]);
-
-        let remaining_amount = u64::from_le_bytes(data[56056..56064].try_into().unwrap());
-        let num_fills = u64::from_le_bytes(data[56064..56072].try_into().unwrap());
-
-        let mut fills = [FilledOrder::default(); 64];
-        for i in 0..64 {
-            let start = 56072 + i * 24;
-            let end = start + 24;
-            fills[i] = FilledOrder::from(&data[start..end]);
-        }
-
-        let num_deltas = u64::from_le_bytes(data[71688..71696].try_into().unwrap());
-
-        let mut node_delta = [NodeDeltaLog::default(); 64];
-        for i in 0..64 {
-            let start = 71696 + i * 56;
-            let end = start + 56;
-            node_delta[i] = NodeDeltaLog::from(&data[start..end]);
-        }
-
-        let amount_cancelled = u64::from_le_bytes(data[87344..87352].try_into().unwrap());
-
-        Self {
-            root_idx,
-            market_buy,
-            nodes,
-            num_orders,
-            current_signer,
-            remaining_amount,
-            num_fills,
-            fills,
-            num_deltas,
-            node_delta,
-            amount_cancelled,
-        }
-    }
-}
-
-
-#[derive(Default)]
+#[derive(Debug, Default, PartialEq, Clone, BorshSerialize, BorshDeserialize)]
 pub struct FeeMod {
     pub base_fee_bp: u64,
     pub collection_fee_bp: u64,
@@ -207,25 +166,7 @@ pub struct FeeMod {
 }
 
 
-impl From<&[u8]> for FeeMod {
-    fn from(data: &[u8]) -> Self {
-        let base_fee_bp = u64::from_le_bytes(data[0..8].try_into().unwrap());
-        let collection_fee_bp = u64::from_le_bytes(data[8..16].try_into().unwrap());
-        let market_maker_fee_bp = u64::from_le_bytes(data[16..24].try_into().unwrap());
-        let dex_fee_bp = u64::from_le_bytes(data[24..32].try_into().unwrap());
-        let collection_royalty_address = Pubkey::new(&data[32..64]);
-
-        Self {
-            base_fee_bp,
-            collection_fee_bp,
-            market_maker_fee_bp,
-            dex_fee_bp,
-            collection_royalty_address,
-        }
-    }
-}
-
-#[derive(Default)]
+#[derive(Debug, Default, PartialEq, Clone, BorshSerialize, BorshDeserialize)]
 pub struct AdditionalPdaAccount {
     pub price: u64,
     // ! deprecated
@@ -236,20 +177,3 @@ pub struct AdditionalPdaAccount {
 }
 
 
-impl From<&[u8]> for AdditionalPdaAccount {
-    fn from(data: &[u8]) -> Self {
-        let price = u64::from_le_bytes(data[0..8].try_into().unwrap());
-        let mint = Pubkey::new(&data[8..40]);
-        let multiplier = u64::from_le_bytes(data[40..48].try_into().unwrap());
-        let quote_mint = Pubkey::new(&data[48..80]);
-        let fee_receiver_wallet = Pubkey::new(&data[80..112]);
-
-        Self {
-            price,
-            mint,
-            multiplier,
-            quote_mint,
-            fee_receiver_wallet,
-        }
-    }
-}
